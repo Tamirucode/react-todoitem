00fb7f9f0eea5004639c50da0748cfe89a75d601
Tamirucode <tam.egu76@gmail.com>
2023-10-10 18:45:55 +0000

    adding CSS to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        
+        <Navbar.Brand>
+          <i class='fa-solid fa-list' height='45'></i>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -14,10 +16,10 @@ const NavBar = () => {
               <i className="fas fa-home"></i>Home
             </Nav.Link>
             <Nav.Link>
-              Sign in
+            <i className="fas fa-sign-in-alt"></i>Sign in
             </Nav.Link>
             <Nav.Link>
-              Sign up
+            <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
