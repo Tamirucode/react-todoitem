37af317cfdf2eda538605d4f067987ea5d45c40a
Tamirucode <tam.egu76@gmail.com>
2023-10-11 21:37:33 +0000

    added signup features to the project

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drf-todo2023-76af1f9fdada.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
