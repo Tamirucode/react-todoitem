f47b96809562a93679d81a924b49f45dcb3fa011
Tamirucode <tam.egu76@gmail.com>
2023-10-12 19:19:42 +0000

    added axios interceptors to refresh access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-todo2023-76af1f9fdada.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
