44699b20d6440659c6fe43e0ad8ba32f90f3b435
Tamirucode <tam.egu76@gmail.com>
2023-10-13 15:03:52 +0000

    created custom hook to handle closing NavBar dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,13 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -23,13 +26,13 @@ const NavBar = () => {
     }
   };
 
-  const addToDoListIcon = (
+  const addPostIcon = (
     <NavLink
       className={styles.NavLink}
       activeClassName={styles.Active}
-      to="/todolists/create"
+      to="/posts/create"
     >
-      <i className="far fa-plus-square"></i>Add todolist
+      <i className="far fa-plus-square"></i>Add post
     </NavLink>
   );
   const loggedInIcons = (
@@ -44,9 +47,9 @@ const NavBar = () => {
       <NavLink
         className={styles.NavLink}
         activeClassName={styles.Active}
-        to="/TodoItem"
+        to="/liked"
       >
-        <i className="fas fa-soild fa-list"></i>TodoItem
+        <i className="fas fa-heart"></i>Liked
       </NavLink>
       <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
         <i className="fas fa-sign-out-alt"></i>Sign out
@@ -79,15 +82,24 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-        {currentUser && addToDoListIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {currentUser && addPostIcon}
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
