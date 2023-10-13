cd48325073727a31c97293abfd52bf6620fee556
Tamirucode <tam.egu76@gmail.com>
2023-10-13 19:02:43 +0000

    created a basic TodDoListPage to display todolist

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import ToDoListCreateForm from "./pages/todolists/ToDoListCeateForm";
+import ToDoListPage from "./pages/todolists/ToDoListPage";
 function App() {
   return (
     <div className={styles.App}>
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/todolists/create" render={() => <ToDoListCreateForm />} />
+          <Route exact path="/todolists/:id" render={() => <ToDoListPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
