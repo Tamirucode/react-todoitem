514e05b4b738ff811aecb0d5ffc61dbd608dd138
Tamirucode <tam.egu76@gmail.com>
2023-10-29 06:48:12 +0000

    added profile component to the project

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+
+
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+
+
+const Profile = (props) => {
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, image, owner } = profile;
+
+ 
+  return (
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <h3 className="m-2">{profile?.owner} <Avatar src={profile_image} /></h3>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/pages/profiles/ProfilePage.js
@@ -0,0 +1,35 @@
+
+
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Avatar from "../../components/Avatar";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+const ProfilePage = () => {
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const profile =  currentUser
+  
+return (
+    <div>
+        <Col lg={6}>
+          <h3 className="m-2">{profile?.owner} <Avatar src={profile_image} /></h3>
+          <Row className="justify-content-center no-gutters">
+            <Col xs={3} className="my-2">
+              <div>{profile?.todolist_count}</div>
+              <div>ToDoList</div>
+            </Col>
+            <Col xs={3} className="my-2">
+              <div>{profile?.todoitem_count}</div>
+              <div>ToDoItem</div>
+            </Col>
+            
+          </Row>
+        </Col>
+    
+
+    </div>
+  )
+}
+
+export default ProfilePage
